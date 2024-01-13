9ba6c53e42bef3f7b86f0830ce35eaaaa026184e
yumin-jung <jungym887@naver.com>
2024-01-13 12:38:13 +0900

    feat: R3F - Geometry, Material 실습

--- src/components/MainCanvas.jsx
+++ src/components/MainCanvas.jsx
@@ -1,9 +1,12 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Color } from "three";
+import { Meshes } from "./Meshes";
 
 export const MainCanvas = () => {
     return (
         <Canvas gl={{ antialias: true }}
+            shadows={"soft"}
             camera={{
                 fov: 60,
                 aspect: window.innerWidth / window.innerHeight,
@@ -13,10 +16,21 @@ export const MainCanvas = () => {
             }}
             scene={{ background: new Color(0x000000) }}
         >
-            <mesh>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshBasicMaterial color={0xff0000} />
-            </mesh>
+            <OrbitControls />
+            <directionalLight
+                castShadow
+                args={[0xffffff, 5]}
+                position={[4, 4, 4]}
+                shadow-camera-left={-25}
+                shadow-camera-right={25}
+                shadow-camera-top={25}
+                shadow-camera-bottom={-25}
+                shadow-camera-near={0.1}
+                shadow-camera-far={1000}
+                shadow-mapSize-width={4096}
+                shadow-mapSize-height={4096}
+            />
+            <Meshes />
         </Canvas>
     );
 }
---
+++ src/components/Meshes.jsx
@@ -0,0 +1,109 @@
+import { Box, Circle, Cone, Plane, Sphere, Cylinder, Torus, TorusKnot } from "@react-three/drei"
+import * as  THREE from "three"
+
+export const Meshes = () => {
+    return (
+        <>
+            {/* <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshBasicMaterial color={0xff0000} />
+            </mesh> */}
+            // drei를 사용하면 내부적으로 bufferGeometry로 구현되므로
+            // GPU를 더 효율적으로 사용하게 되어 성능, 코드적 이점이 있음
+            <Plane args={[40, 40]} rotation-x={-Math.PI / 2} receiveShadow>
+                <meshStandardMaterial />
+            </Plane>
+
+            {/* <Box args={[1, 1, 1]} castShadow position-y={0.5}>
+                <meshStandardMaterial color={0xff0000} />
+            </Box>
+
+            <Sphere
+                args={[1]}
+                position={[0, 1, 3]}
+                material-color={0xffff00}
+            />
+
+            <Circle
+                args={[1]}
+                position={[0, 1, -3]}
+                material-color={"violet"}
+                material-side={THREE.DoubleSide}
+            />
+
+            <Cone
+                args={[1, 2]}
+                position={[3, 1, 3]}
+                material-color={"brown"}
+            />
+
+            <Cylinder
+                args={[2, 1, 2]}
+                position={[3, 1, -3]}
+                material-color={"pink"}
+            />
+
+            <Torus
+                args={[1, 0.2]}
+                position={[-3, 1.2, -3]}
+                material-color={"hotpink"}
+            /> */}
+
+            <TorusKnot
+                args={[1, 0.2, 128, 128, 2, 3]}
+                position={[-3, 1.6, 0]}
+                material-color={"teal"}
+                castShadow
+                receiveShadow
+            >
+                <meshStandardMaterial
+                    color={0xff0000}
+                    roughness={0.5}
+                    metalness={1}
+                />
+            </TorusKnot>
+
+            <TorusKnot
+                args={[1, 0.2, 128, 128, 2, 3]}
+                position={[-7, 1.6, 0]}
+                material-color={"teal"}
+                castShadow
+                receiveShadow
+            >
+                <meshLambertMaterial
+                    color={0x0abff0}
+                    emissive={0xff0000}
+                    emissiveIntensity={0.5}
+                />
+            </TorusKnot>
+
+            <TorusKnot
+                args={[1, 0.2, 128, 128, 2, 3]}
+                position={[-11, 1.6, 0]}
+                material-color={"teal"}
+                castShadow
+                receiveShadow
+            >
+                <meshPhongMaterial
+                    color={0xff0000}
+                    emissive={0x00ff00}
+                    emissiveIntensity={0.5}
+                    specular={0x0000ff}
+                    shininess={100}
+                />
+            </TorusKnot>
+
+            <TorusKnot
+                args={[1, 0.2, 128, 128, 2, 3]}
+                position={[-15, 1.6, 0]}
+                material-color={"teal"}
+                castShadow
+                receiveShadow
+            >
+                <meshDepthMaterial
+                    opacity={0.5}
+                />
+            </TorusKnot>
+        </>
+    )
+}
